568f877fb7c2bedc4a8923c1893aaa4b57fd51a8
Андрей <andrews.ru83@gmail.com>
2022-04-28 15:26:22 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,22 @@ public class Main {
         float [] second = new float[] {1.57f, 7.654f, 9.986f} ;
         int[] third = {2, 6, 10, 15};
         //System.out.println(first.length);
-        for (int i = 0; i < first.length; i++) {
-          if (i == first.length - 1) {
+        for (int i = first.length - 1; i >= 0; i--) {
+          if (i == 0) {
                 System.out.print(first[i]); }
             else {
                 System.out.print(first[i] + ", ");
         }}
         System.out.println();
-            for (int a = 0; a < second.length; a++) {
-          if (a == second.length - 1) {
+            for (int a = second.length - 1; a >= 0; a--) {
+          if (a == 0) {
                 System.out.print(second[a]); }
             else {
               System.out.print(second[a] + ", ");
           }}
               System.out.println();
-            for (int b = 0; b < third.length; b++) {
-          if (b == third.length - 1) {
+            for (int b = third.length - 1; b >= 0 ; b--) {
+          if (b == 0) {
                 System.out.print(third[b]); }
             else {
                 System.out.print(third[b] + ", ");

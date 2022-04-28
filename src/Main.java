1cf7130b0cbb321d59ebc9f01a187ff3b5663875
Андрей <andrews.ru83@gmail.com>
2022-04-28 15:17:39 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,24 @@ public class Main {
         int [] first = new int[] {1,2,3};
         float [] second = new float[] {1.57f, 7.654f, 9.986f} ;
         int[] third = {2, 6, 10, 15};
-           }
-}
+        //System.out.println(first.length);
+        for (int i = 0; i < first.length; i++) {
+          if (i == first.length - 1) {
+                System.out.print(first[i]); }
+            else {
+                System.out.print(first[i] + ", ");
+        }}
+        System.out.println();
+            for (int a = 0; a < second.length; a++) {
+          if (a == second.length - 1) {
+                System.out.print(second[a]); }
+            else {
+              System.out.print(second[a] + ", ");
+          }}
+              System.out.println();
+            for (int b = 0; b < third.length; b++) {
+          if (b == third.length - 1) {
+                System.out.print(third[b]); }
+            else {
+                System.out.print(third[b] + ", ");
+           }}}}
